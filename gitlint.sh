4fd4e82350a1a834ce8fb5a047400d8fa676c060
Scott Abraham <scott.abraham@cerner.com>
2017-08-07 14:26:35 -0500

    Removed react folder requirement and added file extension filter

--- gitlint.sh
+++ gitlint.sh
@@ -2,14 +2,17 @@
 # Runs ESLint against all changed files for the branch.
 
 if [ $# -eq 0 ]; then
-	echo "Comparing with Master"
-	compareTo=master
+  echo "Comparing with Master"
+  compareTo=master
 else
-	echo "Comparing with $1"
-	compareTo=$1
+  echo "Comparing with $1"
+  compareTo=$1
 fi
 
-git diff --name-only $compareTo . | while read line; do
-	echo "Checking $line"
-	./node_modules/.bin/eslint --fix $line
+git diff --name-only $compareTo . | while read file; do
+  # Only lint js and jsx files
+  if [[ $file =~ .*\.jsx*$ ]]; then
+    echo "Checking $file"
+    ./node_modules/.bin/eslint --fix $file
+  fi
 done
